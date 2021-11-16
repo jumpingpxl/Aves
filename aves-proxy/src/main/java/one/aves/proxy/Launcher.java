060183cb530a5538a1f18dc308fe288e30a69f2a
JumpingPxl <jumpingpxl@gmail.com>
2021-11-16 19:16:47 +0100

    Implement ConsoleLogger in Launcher

--- aves-proxy/src/main/java/one/aves/proxy/Launcher.java
+++ aves-proxy/src/main/java/one/aves/proxy/Launcher.java
@@ -1,8 +1,20 @@
 package one.aves.proxy;
 
+import one.aves.api.console.ConsoleLogger;
+
+import java.text.DecimalFormat;
+
 public class Launcher {
 
+	private static final ConsoleLogger LOGGER = ConsoleLogger.getLogger(Launcher.class);
+
 	public static void main(String[] args) {
-		new Aves();
+		long startTime = System.currentTimeMillis();
+		LOGGER.printInfo("Starting Aves Proxy...");
+
+		new Aves().start();
+
+		double bootTime = (System.currentTimeMillis() - startTime) / 1000D;
+		LOGGER.printInfo("Done in {0} seconds!", new DecimalFormat("#.##").format(bootTime));
 	}
 }
